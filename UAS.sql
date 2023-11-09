8834bb7f7e3f2c2d4e24fa53c73877a6a75f29c0
rcruanes <148401359+rcruanes@users.noreply.github.com>
2023-11-09 13:20:30 -0800

    Update UAS.sql

--- UAS.sql
+++ UAS.sql
@@ -1,27 +1,21 @@
-CREATE TABLE education (
-    e_gpa  decimal(3,0) not null,
-    e_schoolname       varchar(55) not null,
-    e_extracurriculars    varchar(152),
-    e_username varchar(152)
-);
 CREATE TABLE users (
     u_username  varchar(152) not null,
     u_email       varchar(50) not null,
     u_password  varchar(50) not null,
-);
-CREATE TABLE demographics (
-    d_income     decimal(10,0) not null,
-    d_ethnicity        varchar(55) not null,
-    d_race       varchar(55) not null,
-    d_age       decimal(3,0) not null,
-    d_gender        varchar(55) not null,
-    d_sex        varchar(55) not null,
-    d_username   varchar(55) not null,
+    u_income     decimal(10,0) not null,
+    u_ethnicity        varchar(55) not null,
+    u_race       varchar(55) not null,
+    u_age       decimal(3,0) not null,
+    u_gender        varchar(55) not null,
+    u_sex        varchar(55) not null,
+    u_gpa  decimal(3,0) not null,
+    u_schoolname       varchar(55) not null,
+    u_extracurriculars    varchar(152)
 );
 CREATE TABLE applications (
     a_appkey     decimal(8,0) not null,
     a_username        varchar(55) not null,
-    a_schoolname     varchar(55) not null,
+    a_schoolname     varchar(55) not null
 );
 CREATE TABLE schools (
     s_schoolname     varchar(55) not null,
@@ -31,17 +25,17 @@ CREATE TABLE schools (
     s_type  varchar(55) not null
 );
 CREATE TABLE major (
+    m_schoolname   varchar(55) not null,
     m_major     varchar(55) not null,
     m_minor        varchar(55),
-    m_athletics     varchar(55),
-    m_schoolname   varchar(55) not null,
+    m_athletics     varchar(55)
 );
 CREATE TABLE essays (
-    es_essay       varchar(55) not null,
-    e_appkey       decimal(9,0) not null,
+    es_essay       varchar(152) not null,
+    e_appkey       decimal(9,0) not null
 );
 CREATE TABLE recommendations (
     r_professors    varchar(55) not null,
     r_employment     varchar(55) not null,
-    r_appkey     decimal(8,0) not null,
+    r_appkey     decimal(8,0) not null
 );
