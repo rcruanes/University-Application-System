9f006b36777be762c97e2d136aa0aec7601181cf
rcruanes <148401359+rcruanes@users.noreply.github.com>
2023-11-09 16:23:59 -0800

    Create 24.sql

---
+++ sql_statements/24.sql
@@ -0,0 +1,2 @@
+INSERT INTO users (u_username, u_email, u_password, u_income, u_ethnicity, u_race, u_age, u_gender, u_sex, u_gpa, u_schoolname, u_extracurriculars)
+VALUES ('Fake_Raph', 'Fake_Raph@gmail.com', 'new_password', 50000, 'Caucasian', 'White', 25, 'Male', 'Male', 3.5, 'Example University', 'Sports Club, Chess Club');
