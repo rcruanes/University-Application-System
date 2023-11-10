9e89b351d424c8b88bb3f1894c619602d65fc4fd
rcruanes <148401359+rcruanes@users.noreply.github.com>
2023-11-09 16:16:15 -0800

    Update 21.sql

--- sql_statements/21.sql
+++ sql_statements/21.sql
@@ -1,2 +1,2 @@
 INSERT INTO applications (a_appkey, a_username, a_schoolname, a_degreekey)
-VALUES (12345678, 'username123', 'Example University', Major#5432);
+VALUES (12345678, 'username123', 'Example University', 'Major#5432');
